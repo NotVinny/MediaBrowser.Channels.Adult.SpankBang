a566c8e844d2a46a34a66e330f7d73b9f058a989
NotVinny <notvinny88@gmail.com>
2018-12-08 13:47:23 -0500

    Initial check-in.
    Plugin exists, but does nothing yet.

---
+++ MediaBrowser.Channels.Adult.SpankBang/Configuration/PluginConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace MediaBrowser.Channels.Adult.SpankBang
+{
+	class PluginConfiguration : BasePluginConfiguration
+	{
+	}
+}
---
+++ MediaBrowser.Channels.Adult.SpankBang/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace MediaBrowser.Channels.Adult.SpankBang
+{
+	class Plugin : BasePlugin<PluginConfiguration>
+	{
+		public override Guid Id => new Guid("7C6DA777-9FB3-45D0-86FD-C171DE6435CB");
+		public override string Name => "SpankBang";
+
+		public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) :
+			base(applicationPaths, xmlSerializer) { }
+	}
+}
